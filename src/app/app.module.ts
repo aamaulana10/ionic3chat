8a46a3040a10715bdbed2321740e4fa554a77389
maulana <aamaulanabola10@gmail.com>
2017-08-02 12:53:30 +0700

    1stcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,36 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ChatPage } from '../pages/chat/chat';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+    var config = {
+        apiKey: "AIzaSyAEjcpBhEBW9WqkZoAxL-aym01Ij4p9GvI",
+        authDomain: "ionic3chat-f3174.firebaseapp.com",
+        databaseURL: "https://ionic3chat-f3174.firebaseio.com",
+        projectId: "ionic3chat-f3174",
+        storageBucket: "ionic3chat-f3174.appspot.com",
+        messagingSenderId: "432650087827"
+      };
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
